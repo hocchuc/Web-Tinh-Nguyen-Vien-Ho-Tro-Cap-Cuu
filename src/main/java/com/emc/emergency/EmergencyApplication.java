5ef6dcc0ea855842f744cf05dea63bb7781a3ccc
Hoc Chuc <hocanhchuc@gmail.com>
2017-05-17 20:50:01 +0700

    Khoi tao lan dau tien, add dependency vao gradle

---
+++ src/main/java/com/emc/emergency/EmergencyApplication.java
@@ -0,0 +1,12 @@
+package com.emc.emergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyApplication.class, args);
+	}
+}
