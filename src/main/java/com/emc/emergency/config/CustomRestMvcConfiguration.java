2a8ca2331b1009088f8a2443d7e19f65f80cf6d0
Hoc Chuc <hocanhchuc@gmail.com>
2017-06-04 11:10:27 +0700

    Heroku incomming

--- src/main/java/com/emc/emergency/config/CustomRestMvcConfiguration.java
+++ src/main/java/com/emc/emergency/config/CustomRestMvcConfiguration.java
@@ -1,10 +1,14 @@
 package com.emc.emergency.config;
 
+import com.emc.emergency.data.model.Accident;
+import com.emc.emergency.data.model.Chat;
+import com.emc.emergency.data.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 class CustomRestMvcConfiguration {
@@ -17,6 +21,11 @@ class CustomRestMvcConfiguration {
       @Override
       public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.setBasePath("/api");
+        config.exposeIdsFor(Accident.class);
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Chat.class);
+        config.setReturnBodyForPutAndPost(true);
+
       }
     };
   }
