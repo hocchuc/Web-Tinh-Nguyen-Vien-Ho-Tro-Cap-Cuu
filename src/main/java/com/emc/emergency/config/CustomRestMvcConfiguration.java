79bc7000d19bb0cd46495576144b4dc86f28c4f0
hocchuc <hocanhchuc@gmail.com>
2017-06-09 15:47:13 -0700

    nhập thêm cmnd

--- src/main/java/com/emc/emergency/config/CustomRestMvcConfiguration.java
+++ src/main/java/com/emc/emergency/config/CustomRestMvcConfiguration.java
@@ -1,11 +1,12 @@
 package com.emc.emergency.config;
 
+import com.emc.emergency.data.model.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 class CustomRestMvcConfiguration {
@@ -18,9 +19,16 @@ class CustomRestMvcConfiguration {
       @Override
       public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.setBasePath("/api");
-        config.setDefaultMediaType(MediaType.APPLICATION_JSON_UTF8);
-        config.setReturnBodyForPutAndPost(true);
+        config.exposeIdsFor(Accident.class);
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Chat.class);
+        config.exposeIdsFor(Personal_Infomation.class);
+        config.exposeIdsFor(Medical_Info.class);
+        config.exposeIdsFor(User_Type.class);
+        config.exposeIdsFor(Image.class);
 
+
+        config.setReturnBodyForPutAndPost(true);
       }
     };
   }
