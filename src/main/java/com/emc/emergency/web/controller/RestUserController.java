eaa88ce49f716574bc8320db6d862230b721fd34
hocanhchuc@gmail.com <hocanhchuc@gmail.com>
2017-06-19 10:26:38 +0700

    new update, stable local with register responge user_id

--- src/main/java/com/emc/emergency/web/controller/RestUserController.java
+++ src/main/java/com/emc/emergency/web/controller/RestUserController.java
@@ -29,11 +29,19 @@ public class RestUserController {
     @Autowired
     userRepository userRepository;
 
-    @RequestMapping(value = "/api/login", method = RequestMethod.POST,consumes = MediaType.ALL_VALUE,produces = "application/json")
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE,produces = "application/json")
     @ResponseBody
     public FlashMessage Login (@RequestBody User user) {
         FlashMessage flashMessage = new FlashMessage(LOGIN,"login",FAILURE);
         if(userService.Login(user.getUsername(),user.getPassword())) {
+            User user1 = userRepository.findByUsername(user.getUsername());
+            if(user1 !=null) {
+                user1.setLat_PI(user.getLat_PI());
+                user1.setLong_PI(user.getLong_PI());
+                userRepository.save(user1);
+            }
+
             flashMessage.setStatus(SUCCESS);
             flashMessage.setMessage((userRepository.findByUsername(user.getUsername()).getId_user())+"");
         }
@@ -44,13 +52,45 @@ public class RestUserController {
     @ResponseStatus(HttpStatus.CREATED)
     public FlashMessage Register (@RequestBody User user) {
         FlashMessage flashMessage = new FlashMessage(REGISTER,"register",FAILURE);
-        if(userService.Register(user.getUsername(),user.getPassword())) flashMessage.setStatus(SUCCESS);
+        if(userService.Register(user.getUsername(),user.getPassword())) {
+            flashMessage.setStatus(SUCCESS);
+            flashMessage.setMessage(userRepository.findByUsername(user.getUsername()).getId_user()+"");
+
+        }
+        return flashMessage;
+    }
+    @RequestMapping(value = "/api/refreshToken", method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE,produces = "application/json")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public FlashMessage refreshToken (@RequestParam(value="token") String token,
+                                      @RequestParam(value="id_user") Long id_user) {
+        FlashMessage flashMessage = new FlashMessage(TOKEN,"refreshToken",FAILURE);
+        User user = userRepository.findOne(Long.valueOf(id_user));
+        if(user!=null){
+            user.setToken(token);
+            userRepository.save(user);
+            flashMessage.setStatus(SUCCESS);
+        }
         return flashMessage;
     }
 
-
-
-
-
+    @RequestMapping(value = "/api/postLocation", method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE,produces = "application/json")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public FlashMessage postLocation (@RequestParam(value="lat") String lat,
+                                      @RequestParam(value="lon") String lon,
+                                      @RequestParam(value="id_user") Long id_user) {
+        FlashMessage flashMessage = new FlashMessage(TOKEN,"refreshToken",FAILURE);
+        User user = userRepository.findOne(Long.valueOf(id_user));
+        if(user!=null){
+            user.setLat_PI(Double.valueOf(lat));
+            user.setLong_PI(Double.valueOf(lon));
+            userRepository.save(user);
+            flashMessage.setStatus(SUCCESS);
+        }
+        return flashMessage;
+    }
 
 }
