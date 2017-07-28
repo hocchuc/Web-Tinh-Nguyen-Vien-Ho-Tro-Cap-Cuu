54363b6e8f3711613992e7021870e65c34b3df49
hocchuc <hocanhchuc@gmail.com>
2017-07-28 11:59:14 +0700

    Preloader + floating button

--- src/main/java/com/emc/emergency/EmergencyApplication.java
+++ src/main/java/com/emc/emergency/EmergencyApplication.java
@@ -4,6 +4,8 @@ import com.emc.emergency.data.bean.CcsOutMessage;
 import com.emc.emergency.util.Util;
 import com.emc.emergency.xmpp.CcsClient;
 import com.emc.emergency.xmpp.MessageHelper;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
 import org.springframework.boot.SpringApplication;
@@ -23,7 +25,10 @@ import java.util.logging.Logger;
 @ComponentScan
 public class EmergencyApplication {
 	public static final Logger logger = Logger.getLogger(EmergencyApplication.class.getName());
-
+		@PostConstruct
+			void started() {
+				TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+			}
 	public static void main(String[] args)throws SmackException, IOException  {
 		SpringApplication.run(EmergencyApplication.class, args);
 
